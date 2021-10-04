9d3e99427c4c56d63ae9dddacd38c18ab357b69c
diegomonroe <diego-monroe@hotmail.com>
2021-10-04 01:15:51 -0600

    Se cambia el main para hacer correr el programa.

--- Tarea/main.cpp
+++ Tarea/main.cpp
@@ -1,25 +1,26 @@
 #include "planilla.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main() {
 
 	Planilla* planilla = new Planilla();
+
+	string archivoPersonas = planilla->convertirArchivo("Personas.txt");
+	planilla->llenarPlanilla(archivoPersonas);
 	
-	planilla->llenarPlanilla();
-	//planilla->agregarSalarios();
-	//planilla->agregarHoras();
-	//cout << *planilla;
+	string archivoSalariosNomina = planilla->convertirArchivo("Nomina.txt");
+	planilla->agregarSalarios(archivoSalariosNomina);
+
+	string archivoPagoHoras = planilla->convertirArchivo("HorasTrabajadas.txt");
+	planilla->agregarHoras(archivoSalariosNomina);
 
 	std::ofstream archivoSalida("Reporte.csv", std::ofstream::out);
 	archivoSalida << *planilla;
 	archivoSalida.close();
 
 	delete planilla;
-	
-
-    
-
 	return 0;
 }
