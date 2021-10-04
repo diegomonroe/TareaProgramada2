088a5c166dd6cf2ebc43a97aaf0436de81b80979
diegomonroe <diego-monroe@hotmail.com>
2021-10-04 01:12:50 -0600

    Se agregan tests para la creacion de planilla con solo el director, con dos personas y tres personas, junto con la correcta salida al archivo esperada.

--- Pruebas Tarea 2/test.cpp
+++ Pruebas Tarea 2/test.cpp
@@ -10,29 +10,171 @@
 #include <string>
 #include <sstream>
 using namespace std;
-TEST(PlanillaTest, Test_ConstructorDosTrabajadores) {
-    Planilla* planilla = new Planilla();
-    planilla->agregarEmpleado(1,"Diego","Monroe","1234@prueba.com",1,0);
-    planilla->agregarEmpleado(2, "Angelica", "Curling", "9876@prueba.com", 2, 1);
-    
-    ostringstream streamSalida{};
-    streamSalida << *planilla;
-    string actual = streamSalida.str();
 
-    ostringstream streamSalidaEsperada{};
-    streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
-    streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
-    streamSalidaEsperada << "2,Angelica Curling,Diego Monroe,0.00" << endl;
+namespace Test_Agregar_Empleados{
 
-    streamSalidaEsperada << "Subtotal:,0.00" << endl;
-    streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
-    streamSalidaEsperada << "Total:,0.00" << endl;
+    TEST(PlanillaTest, Test_AgregarSalarioDirector) {
+        Planilla* planilla = new Planilla();
 
+        //Ingreso de archivo
+        ostringstream ingresoDatos;
+        ingresoDatos << "1 Diego Monroe 1234@prueba.com 1 0" << endl;
+        string entradaEjemplo = ingresoDatos.str();
+        planilla->llenarPlanilla(entradaEjemplo);
 
-    string esperada = streamSalidaEsperada.str();
-    
+        //Resultado de la impresion de planilla
+        ostringstream streamSalida{};
+        streamSalida << *planilla;
+        string actual = streamSalida.str();
 
-   delete planilla;
+        //Resultado esperado
+        ostringstream streamSalidaEsperada{};
+        streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
+        streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
+        streamSalidaEsperada << "Subtotal:,0.00" << endl;
+        streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
+        streamSalidaEsperada << "Total:,0.00" << endl;
 
-    EXPECT_EQ(esperada, actual);
+        string esperada = streamSalidaEsperada.str();
+
+        delete planilla;
+
+        EXPECT_EQ(esperada, actual);
+    }
+
+    //Falta modificar
+    TEST(PlanillaTest, Test_ConstructorDosTrabajadores) {
+        Planilla* planilla = new Planilla();
+        planilla->agregarEmpleado(1, "Diego", "Monroe", "1234@prueba.com", 1, 0);
+        planilla->agregarEmpleado(2, "Angelica", "Curling", "9876@prueba.com", 2, 1);
+
+        ostringstream streamSalida{};
+        streamSalida << *planilla;
+        string actual = streamSalida.str();
+
+        ostringstream streamSalidaEsperada{};
+        streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
+        streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
+        streamSalidaEsperada << "2,Angelica Curling,Diego Monroe,0.00" << endl;
+        streamSalidaEsperada << "Subtotal:,0.00" << endl;
+        streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
+        streamSalidaEsperada << "Total:,0.00" << endl;
+        string esperada = streamSalidaEsperada.str();
+
+        delete planilla;
+
+        EXPECT_EQ(esperada, actual);
+    }
+
+    //Falta modificar
+    TEST(PlanillaTest, Test_ConstructorTresTrabajadores) {
+        Planilla* planilla = new Planilla();
+        planilla->agregarEmpleado(1, "Diego", "Monroe", "diego@prueba.com", 1, 0);
+        planilla->agregarEmpleado(2, "Angelica", "Curling", "angelica@prueba.com", 2, 1);
+        planilla->agregarEmpleado(3, "Karol", "Corrales", "karol@prueba.com", 1, 2);
+
+        ostringstream streamSalida{};
+        streamSalida << *planilla;
+        string actual = streamSalida.str();
+
+        ostringstream streamSalidaEsperada{};
+        streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
+        streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
+        streamSalidaEsperada << "2,Angelica Curling,Diego Monroe,0.00" << endl;
+        streamSalidaEsperada << "3,Karol Corrales,Angelica Curling,0.00" << endl;
+        streamSalidaEsperada << "Subtotal:,0.00" << endl;
+        streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
+        streamSalidaEsperada << "Total:,0.00" << endl;
+        string esperada = streamSalidaEsperada.str();
+
+        delete planilla;
+
+        EXPECT_EQ(esperada, actual);
+    }
+
 }
+
+//Falta modificar
+/*
+namespace Test_Agregar_Salarios {
+
+    TEST(PlanillaTest, Test_AgregarSalarioDirector) {
+        Planilla* planilla = new Planilla();
+        
+        //Ingreso de archivo
+        ostringstream ingresoDatos;
+        ingresoDatos << "1 Diego Monroe 1234@prueba.com 1 0" << endl;
+        string entradaEjemplo = ingresoDatos.str();
+        planilla->llenarPlanilla(entradaEjemplo);
+
+        //Resultado de la impresion de planilla
+        ostringstream streamSalida{};
+        streamSalida << *planilla;
+        string actual = streamSalida.str();
+
+        //Resultado esperado
+        ostringstream streamSalidaEsperada{};
+        streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
+        streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
+        streamSalidaEsperada << "Subtotal:,0.00" << endl;
+        streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
+        streamSalidaEsperada << "Total:,0.00" << endl;
+
+        string esperada = streamSalidaEsperada.str();
+
+        delete planilla;
+
+        EXPECT_EQ(esperada, actual);
+    }
+
+    TEST(PlanillaTest, Test_ConstructorDosTrabajadores) {
+        Planilla* planilla = new Planilla();
+        planilla->agregarEmpleado(1, "Diego", "Monroe", "1234@prueba.com", 1, 0);
+        planilla->agregarEmpleado(2, "Angelica", "Curling", "9876@prueba.com", 2, 1);
+
+        ostringstream streamSalida{};
+        streamSalida << *planilla;
+        string actual = streamSalida.str();
+
+        ostringstream streamSalidaEsperada{};
+        streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
+        streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
+        streamSalidaEsperada << "2,Angelica Curling,Diego Monroe,0.00" << endl;
+        streamSalidaEsperada << "Subtotal:,0.00" << endl;
+        streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
+        streamSalidaEsperada << "Total:,0.00" << endl;
+        string esperada = streamSalidaEsperada.str();
+
+        delete planilla;
+
+        EXPECT_EQ(esperada, actual);
+    }
+
+    TEST(PlanillaTest, Test_ConstructorTresTrabajadores) {
+        Planilla* planilla = new Planilla();
+        planilla->agregarEmpleado(1, "Diego", "Monroe", "diego@prueba.com", 1, 0);
+        planilla->agregarEmpleado(2, "Angelica", "Curling", "angelica@prueba.com", 2, 1);
+        planilla->agregarEmpleado(3, "Karol", "Corrales", "karol@prueba.com", 1, 2);
+
+        ostringstream streamSalida{};
+        streamSalida << *planilla;
+        string actual = streamSalida.str();
+
+        ostringstream streamSalidaEsperada{};
+        streamSalidaEsperada << "ID_Empleado, Nombre completo, Nombre completo del supervisor, Monto a pagar(monto neto)" << endl;
+        streamSalidaEsperada << "1,Diego Monroe, ,0.00" << endl;
+        streamSalidaEsperada << "2,Angelica Curling,Diego Monroe,0.00" << endl;
+        streamSalidaEsperada << "3,Karol Corrales,Angelica Curling,0.00" << endl;
+        streamSalidaEsperada << "Subtotal:,0.00" << endl;
+        streamSalidaEsperada << "Impuestos a retener:,0.00" << endl;
+        streamSalidaEsperada << "Total:,0.00" << endl;
+        string esperada = streamSalidaEsperada.str();
+
+        delete planilla;
+
+        EXPECT_EQ(esperada, actual);
+    }
+
+}
+
+*/
